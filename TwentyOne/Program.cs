6c8ca2fe205413e41de37feb7af2bd2599be0b4c
ndubs18 <32852963+ndubs18@users.noreply.github.com>
2018-06-02 10:17:57 -0700

    First Commit

---
+++ TwentyOne/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Casino;
+using Casino.TwentyOne;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string casinoName = "Grand Hotel and Casino";
+
+            Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
+            string playerName = Console.ReadLine();
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                player.Id = Guid.NewGuid();
+                using (StreamWriter file = new StreamWriter(@"C:\Users\jesse\logs\log.txt", true))
+                {
+                    file.WriteLine(player.Id);
+                }
+                Game game = new TwentyOneGame();
+
+                game += player;
+                game.Players.Add(player);
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read();
+        }
+    }
+}
---
+++ TwentyOneWebApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Casino;
+using Casino.TwentyOne;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TwentyOneWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Play(Player player)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", player);
+            }
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players.Add(player);
+            player.isActivelyPlaying = true;
+            player.Hand = new List<Card>();
+            player.Stay = false;
+            game.Dealer = new TwentyOneDealer()
+            {
+                Hand = new List<Card>(),
+                Stay = false,
+                Deck = new Deck(),
+            };
+            game.Dealer.Deck.Shuffle();
+            return View(game);
+        }
+    }
+}
